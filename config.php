03a1bd250fcb31f5bddf44f181e6a57ddb5eda68
bluntboomer <bluntboomer@gmail.com>
2018-06-14 17:29:39 -0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+
+//   _
+//  (_)    A self-hosted ShareX image serving solution.
+//   _ _ __ ___   __ _  __ _  ___  ___  ___ _ ____   _____
+//  | | '_ ` _ \ / _` |/ _` |/ _ \/ __|/ _ \ '__\ \ / / _ \
+//  | | | | | | | (_| | (_| |  __/\__ \  __/ |   \ V /  __/
+//  |_|_| |_| |_|\__,_|\__, |\___||___/\___|_|    \_/ \___|
+//                      __/ |
+//                     |___/   created by github.com/aerouk
+
+/* More information on all these values can be found on the wiki page. */
+/* https://github.com/aerouk/imageserve/wiki/Configuration */
+
+define('RAW_IMAGE', true);
+define('RAW_IMAGE_LINK', false);
+define('IMAGE_EXTENSION', true);
+
+define('TWITTER_CARDS', false);
+
+// If you're using this, make sure to put a forward slash before.
+// E.g. "/imageserve" not "imageserve"
+define('IMAGESERVE_DIR', '');
+define('TWITTER_HANDLE', '@aerouk_');
+
+define('APP_NAME', 'nigger-heroku');
+define('PASSKEY', getenv('PASSKEY'));
